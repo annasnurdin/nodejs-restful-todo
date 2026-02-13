6787ab2009763e852b26421aab7a43c6af0108d7
annasnurdin <anasnurdin02@gmail.com>
2026-02-13 10:49:54 +0700

    update todo / put

--- app.mjs
+++ app.mjs
@@ -3,13 +3,15 @@ import { TodolistService } from './todolist-service.mjs';
 
 const service = new TodolistService()
 const server = http.createServer((request, response) => {
-    
+
     response.setHeader("Content-Type", "application/json")
 
     if (request.method === "GET"){
         service.getTodoList(request, response)
     } else if (request.method === "POST"){
         service.createTodo(request, response)
+    } else if (request.method === "PUT"){
+        service.updateTodo(request, response)
     }
 })
 
