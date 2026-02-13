9a78ff0a60ec95b8d033120fd3965fa2d86389ce
annasnurdin <anasnurdin02@gmail.com>
2026-02-13 10:01:51 +0700

    add initial service

---
+++ todolist-service.mjs
@@ -0,0 +1,21 @@
+export class TodolistService {
+    todolist = ["Annas","Nurdin"];
+
+    getJsonTodoList(){
+        return JSON.stringify({
+            code: 200,
+            status: "OK",
+            data: this.todolist.map((value, index) => {
+                return {
+                    id: index,
+                    todo: value
+                }
+            })
+        })
+    }
+
+    getTodoList(request, response){
+        response.write(this.getJsonTodoList())
+        response.end()
+    }
+}
