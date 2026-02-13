9efd33f8ac6d374eebe768625d7ab3dfa0eb9fdd
annasnurdin <anasnurdin02@gmail.com>
2026-02-13 10:42:51 +0700

    post

--- app.mjs
+++ app.mjs
@@ -3,9 +3,13 @@ import { TodolistService } from './todolist-service.mjs';
 
 const service = new TodolistService()
 const server = http.createServer((request, response) => {
+    
+    response.setHeader("Content-Type", "application/json")
 
     if (request.method === "GET"){
         service.getTodoList(request, response)
+    } else if (request.method === "POST"){
+        service.createTodo(request, response)
     }
 })
 
